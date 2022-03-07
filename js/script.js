275adbc2867dd600bc922fe4b1d3de91b10479e8
Ed <example@gmail.com>
2022-03-07 18:24:23 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// alert('stuli pelku');
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt('Что надо?', '');
+// console.log(answer + 5);
+
+
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваше фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+
+// document.write(answers);
+
+
+
+// const category = 'toys';
+
+// console.log(`https;//someurl.com/${category}/5`);
+
+// const user = 'Ed';
+
+// alert(`Hi, ${user}`);
+
+
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(--incr);
+// console.log(++decr); 
+
+
+
+// console.log(10 % 4);
+// console.log(2 * 4 === 8);
+
+
+// const isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked && isClose);
+
+
+
+
+        
+      
